d0d109051ce9c92b5ba2e05694274533bf3acc82
Arash Mousavi <me@amousavi.dev>
2023-10-20 06:03:38 +0100

    Fix errors on registration page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,17 +3,21 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :configure_permitted_parameters
+    before_action :set_countries, only: %i[new create]
 
     # GET /resource/sign_up
     def new
       page_title('Sign Up')
-      @countries = Countries.new.map { |key, value| [value, key] }
 
       super
     end
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: %i[country city])
+    end
+
+    def set_countries
+      @countries = Countries.new.map { |key, value| [value, key] }
     end
   end
 end
